574e64abed32bd58d1755b2caa8478d6af92e56d
Michack <97833068+html2195@users.noreply.github.com>
2026-01-30 13:18:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const lessons = [
   { title: 'Хімія та Біологія', link: 'https://us05web.zoom.us/j/3305145414?pwd=RWQ5TmpEMjRaSmNxZ0xSNjZlNStEUT09' },
   { title: 'Англійська мова', link: 'https://us05web.zoom.us/j/81165574402?pwd=mNOMR0LqScGubCJHMiZqIhoukgvKVN.1' },
   { title: 'Технології', link: 'https://us05web.zoom.us/j/84299758339?pwd=g8oM9SJmzsGYopt5btIDaMHzDk1of4.1' },
-  { title: 'Фізра', link: 'https://us05web.zoom.us/j/81165574402?pwd=mNOMR0LqScGubCJHMiZqIhoukgvKVN.1' }
+  { title: 'Фізра', link: 'https://us05web.zoom.us/j/81165574402?pwd=mNOMR0LqScGubCJHMiZqIhoukgvKVN.1' },
   { title: 'Операційні системи', link: 'https://us05web.zoom.us/j/3030586616?pwd=j4G03aJIS3htQH2UROr5qx1bbERkmq.1&omn=86085672797' },
 ];
 
